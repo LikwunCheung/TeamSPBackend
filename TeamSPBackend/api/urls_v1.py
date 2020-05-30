9a82fc22538d499882c432cd7860fa6c93efd0aa
Zachary Ho <zyyho97@gmail.com>
2020-05-30 15:00:33 +1000

    Created URLs for subject HTTP methods. Created a function in views/subject.py for getting a subject.

--- TeamSPBackend/api/urls_v1.py
+++ TeamSPBackend/api/urls_v1.py
@@ -2,10 +2,20 @@
 
 from django.urls import path
 from .views.invitation import invitation_router
-from .views.supervisors import supervisors_router
+from .views.subject import subject_router
+from .views import supervisors
 urlpatterns = [
     # Invitation Related API
-    path('subject/<int:id>/invite', invitation_router),
+    path('subject/<int:id>/invite/', invitation_router),
+
     # Invitation Related API
-    path('subject/<int:id>/supervisors', supervisors_router)
+    path('subject/<int:id>/supervisors/', supervisors.supervisors_router)
+    # Add subject
+    path('subject/add/', supervisors.subject_add)
+    # Update subject
+    path('subject/update/', supervisors.subject_update)
+    # Delete subject
+    path('subject/delete/', supervisors.subject_delete)
+    # Get subject
+    path('subject/<int:subject_id>/', supervisors.subject_get)
 ]
--- TeamSPBackend/api/views/subject.py
+++ TeamSPBackend/api/views/subject.py
@@ -11,10 +11,11 @@ from TeamSPBackend.common.utils import make_json_response, init_http_response
 from TeamSPBackend.account.models import Account
 from TeamSPBackend.subject.models import Subject
 
+
 @require_http_methods(['GET', 'DELETE', 'PUT'])
-def supervisorsSubject_router(request, subject_id):
+def subject_router(request, *args, **kwargs):
     if request.method == 'GET':
-        return get_supervisors(subject_id)
+        return get_supervisors('id')
     # elif request.method == 'DELETE':
     #     return delete_supervisors(subject_id)
     # elif request.method == 'PUT':
@@ -23,4 +24,32 @@ def supervisorsSubject_router(request, subject_id):
 
 
 def get_supervisors(id):
-    subject = Subject.objects.get(subject_id = id)
+    subject = Subject.objects.get(subject_id=id)
+
+
+@require_http_methods(['GET'])
+def subject_get(request, subject_id):
+    subject = Subject.objects.filter(subject_id=subject_id)
+    result = [dict(
+        subject_id=subject.subject_id,
+        subject_code=subject.subject_code,
+        subject_name=subject.subject_name,
+        coordinator_id=subject.coordinator_id,
+        supervisors=subject.supervisors,
+        status=subject.status)]
+
+    resp = init_http_success()
+    resp['data'] = result
+    return make_json_response(HttpResponse, resp)
+
+
+@require_http_methods(['POST'])
+def subject_add(request):
+
+
+@require_http_methods(['POST'])
+def subject_update(request):
+
+
+@require_http_methods(['POST'])
+def subject_delete(request):
